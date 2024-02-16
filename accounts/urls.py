74b46973a09ecda024d956ef7ba48e0631a0544b
Harsh <harshbca2021@gmail.com>
2024-02-16 22:14:26 +0530

    vendor registration

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path('registerUser/', views.registerUser, name="registerUser"),
+    path('registerVendor/', views.registerVendor, name="registerVendor"),
 
 ]
